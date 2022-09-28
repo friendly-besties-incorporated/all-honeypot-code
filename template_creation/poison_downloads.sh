211bce4a316ad5ef2b8dac44b65962eaeba0f240
Svetlana Semenova <sysemenova@gmail.com>
2022-09-28 00:01:37 +0000

    Make poison downloads work

--- template_creation/poison_downloads.sh
+++ template_creation/poison_downloads.sh
@@ -10,3 +10,50 @@ fi
 # and was wrong, so someone else's should be placed here
 
 # You can place the poisoned wget and curl files into the "poisoned_versions" folder
+# Check container exist
+if ! sudo lxc-ls | grep -q -w "$1"
+then
+  echo "$1" "doesn't exist."
+  exit 2
+fi
+# Check container running
+if ! sudo lxc-info -n "$1" | grep -q -w "RUNNING"
+then
+  echo "$1" "isn't running."
+  exit 3
+fi
+# If we're here, then we can now safely start making things!
+# Helpful~
+tocont=/var/lib/lxc/"$1"/rootfs
+
+# wget section
+tocheck="$tocont"/usr/bin/wwget
+# Check if this script has already been run
+if sudo test -f "$tocheck"
+then
+  echo "Poisoned wget already exists."
+else
+  # make the necessary directory
+  sudo mkdir -p "$tocont"/var/log/.downloads
+  # Install wget
+  sudo lxc-attach -n "$1" -- bash -c "sudo apt-get install wget"
+  # Change name and stick fakewget into the wget spot
+  sudo mv "$tocont"/usr/bin/wget "$tocont"/usr/bin/wwget
+  sudo cp ./poisoned_versions/fakewget "$tocont"/usr/bin/wget
+fi
+
+# wget section
+tocheck="$tocont"/usr/bin/ccurl
+# Check if this script has already been run
+if sudo test -f "$tocheck"
+then
+  echo "Poisoned curl already exists on this container."
+else
+  # make the necessary directory
+  sudo mkdir -p "$tocont"/var/log/.downloads
+  # Install wget
+  sudo lxc-attach -n "$1" -- bash -c "sudo apt-get install curl"
+  # Change name and stick fakewget into the wget spot
+  sudo mv "$tocont"/usr/bin/curl "$tocont"/usr/bin/ccurl
+  sudo cp ./poisoned_versions/fakecurl "$tocont"/usr/bin/curl
+fi
