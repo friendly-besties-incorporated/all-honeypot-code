e462a520dd610bc344d1bf93ef9b118d8127ed79
Svetlana Semenova <sysemenova@gmail.com>
2022-10-11 18:37:54 +0000

    Remove firewall change

--- running_hpot_code/start_all_honeypots.sh
+++ running_hpot_code/start_all_honeypots.sh
@@ -6,8 +6,8 @@
 # four times
 
 # Ensure that host firewall rules are present
-sudo /bin/bash ./start_up/basic_firewall_rules.sh
-
+#sudo /bin/bash ./start_up/basic_firewall_rules.sh
+source honeypot_environment
 # use CNAME_CONTROL, CNAME_DITINFO, CNAME_DIDPSWD, and CNAME_IOTPAGE for the template names
 
 /bin/bash ./serve_honeypot.sh $CNAME_CONTROL $IP_CONTROL more
