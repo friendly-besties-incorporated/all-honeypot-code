fa0a49eb76ce30584b8e8337b7fcc78ceb1da10a
D.J. Figueroa <djfigs10@gmail.com>
2022-10-11 18:27:43 +0000

    add hostname change

--- running_hpot_code/cron_jobs/ICMP_collector.sh
+++ running_hpot_code/cron_jobs/ICMP_collector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-interface="enth1pormsthn"
+interface=""
 logpath="$H_P_LOGSTORE""/icmp"
 
 control_path="$logpath""/""$CNAME_CONTROL"
@@ -10,7 +10,7 @@ iotpage_path="$logpath""/""$CNAME_IOTPAGE"
 
 sudo mkdir -p "$control_path" "$ditinfo_path" "$didpswd_path" "$iotpage_path"
 
-sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_CONTROL" -G 86400 -w "$control_path""/icmplog-%m-%d"
-sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_DITINFO" -G 86400 -w "$ditinfo_path""/icmplog-%m-%d"
-sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_DIDPSWD" -G 86400 -w "$didpswd_path""/icmplog-%m-%d"
-sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_IOTPAGE" -G 86400 -w "$iotpage_path""/icmplog-%m-%d"
+sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_CONTROL" -G 86400 -w "$control_path""/icmplog-%m-%d" &
+sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_DITINFO" -G 86400 -w "$ditinfo_path""/icmplog-%m-%d" &
+sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_DIDPSWD" -G 86400 -w "$didpswd_path""/icmplog-%m-%d" &
+sudo tcpdump -i "$interface" 'icmp port 80 or icmp port 443' dst "$IP_IOTPAGE" -G 86400 -w "$iotpage_path""/icmplog-%m-%d" &
