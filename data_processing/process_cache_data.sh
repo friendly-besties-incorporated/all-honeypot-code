25abab70fa78437539b9e442f2f82b3047b5179d
Annabel Yang <90069688+ayy05@users.noreply.github.com>
2022-11-12 13:02:28 -0500

    added rejected files

--- data_processing/process_cache_data.sh
+++ data_processing/process_cache_data.sh
@@ -16,44 +16,47 @@ for file in $(find $dir -name '*.log.gz')
 do
     if [ $(cat $processed | grep -c $file) -eq 0 ]
     then
-        echo $file >> $processed
-
-        #------------------------------------------------------------
-        # MITM SESSION LOG DATA
-        container=$(zcat $file| head -n 1 | cut -d" " -f3)
-        attackerIP=$(zcat $file| head -n 3 | tail -n 1 | cut -d" " -f3)
-        startTime=$(zcat $file | head -n 4 | tail -n 1 | cut -d" " -f3,4)
-
-        noninteractive=$(zcat $file | grep -c "Noninteractive mode attacker command")
-        if [ $noninteractive -eq 1 ]
+        if [ $(echo $file | grep -c REJECTED) -eq 0 ]
         then
-            noninteractive="y"
-        else
-            noninteractive="n"
+            echo $file >> $processed
+
+            #------------------------------------------------------------
+            # MITM SESSION LOG DATA
+            container=$(zcat $file| head -n 1 | cut -d" " -f3)
+            attackerIP=$(zcat $file| head -n 3 | tail -n 1 | cut -d" " -f3)
+            startTime=$(zcat $file | head -n 4 | tail -n 1 | cut -d" " -f3,4)
+
+            noninteractive=$(zcat $file | grep -c "Noninteractive mode attacker command")
+            if [ $noninteractive -eq 1 ]
+            then
+                noninteractive="y"
+            else
+                noninteractive="n"
+            fi
+
+            username=$(zcat $file | head -n 9 | grep "Attacker Username" | cut -d" " -f3)
+            password=$(zcat $file | head -n 9 | grep "Attacker Password" | cut -d" " -f3)
+
+            #------------------------------------------------------------
+
+            # COMMANDS DATA
+
+            commandsNum=0
+
+            while read -r cmd
+            do
+                ((commandsNum++))
+                echo "$container | $attackerIP | $startTime | $username | $password | $noninteractive | $cmd" >> $cmds_output
+            done < <(zcat $file | grep -w "Noninteractive\|line" | sed 's/.*: //' | tr "['||','&&',';']" "\n" | sed '/^$/d' | sed 's/^[ ]//')
+
+
+            #------------------------------------------------------------
+
+            # MITM OUTPUT
+            # Each line has data for each attacker, delimited by a | character.
+            echo "$container | $attackerIP | $startTime | $username | $password | $commandsNum | $noninteractive" >> $output_file
+
+            sudo mv $file /var/experiment_logs/prev_cache
         fi
-
-        username=$(zcat $file | head -n 9 | grep "Attacker Username" | cut -d" " -f3)
-        password=$(zcat $file | head -n 9 | grep "Attacker Password" | cut -d" " -f3)
-
-        #------------------------------------------------------------
-
-        # COMMANDS DATA
-
-        commandsNum=0
-
-        while read -r cmd
-        do
-            ((commandsNum++))
-            echo "$container | $attackerIP | $startTime | $username | $password | $noninteractive | $cmd" >> $cmds_output
-        done < <(zcat $file | grep -w "Noninteractive\|line" | sed 's/.*: //' | tr "['||','&&',';']" "\n" | sed '/^$/d' | sed 's/^[ ]//')
-
-
-        #------------------------------------------------------------
-
-        # MITM OUTPUT
-        # Each line has data for each attacker, delimited by a | character.
-        echo "$container | $attackerIP | $startTime | $username | $password | $commandsNum | $noninteractive" >> $output_file
-        
-        sudo mv $file /var/experiment_logs/prev_cache
     fi
 done
